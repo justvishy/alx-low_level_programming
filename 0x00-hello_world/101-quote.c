35d7c71cb90a60b748b8c9e63394bf1ce37bb045
Vishnu <justvishy@gmail.com>
2023-10-26 06:29:42 -0700

    Submitting my first c program advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is a main function
+ * Return: 1
+ */
+
+int main(void)
+{
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int a = 0;
+
+	while (string[a] != '\0')
+	{
+		putchar(string[a]);
+		a++;
+	}
+
+	return (1);
+}
