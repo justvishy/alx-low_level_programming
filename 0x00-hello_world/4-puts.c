818d919e303d31afd310ad634f96d4b58a924578
Vishnu <justvishy@gmail.com>
2023-10-26 05:22:57 -0700

    pushing my first C programming program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Return: A string
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
