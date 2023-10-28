9670bb7e7ef6fdbb869a0021a2c124c99a454fbc
Vishnu <justvishy@gmail.com>
2023-10-28 00:56:35 -0700

    This is my seventh program of this project

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this is a main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int alpha = 97;
+
+	while (alpha <= 122)
+	{
+		if (alpha == 101 || alpha == 113)
+		{
+			alpha++;
+		}
+		putchar(alpha);
+		alpha++;
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is a main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
