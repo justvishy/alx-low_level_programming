5af19e6a58825628779ac7c36c0ae418ea2689e6
Vishnu <justvishy@gmail.com>
2023-10-28 00:04:55 -0700

    This is my third c program of this project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is a main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int alpha = 97;
+
+	while (alpha <= 122)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
