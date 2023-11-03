fbfea829c0840c50d98c952387b8098325433e03
Vishnu <justvishy@gmail.com>
2023-11-03 05:48:32 -0700

    This fourth c program for this project

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - this prints 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
