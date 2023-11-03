2f4db9fddb1e5d264318c5d9d285abc4f8d41fd2
Vishnu <justvishy@gmail.com>
2023-11-03 05:43:25 -0700

    This is the third c program of this project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - this finds the multiple of two numbers
+ * @a: it is the first input
+ * @b: it is the second input
+ * Return: Multiple
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
