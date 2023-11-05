dea5fc10e25ebf0e26e885170b00523d3e67a23b
Vishnu <justvishy@gmail.com>
2023-11-05 03:32:35 -0800

    This is my sixth c program of this project

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - this finds the upper character
+ * @n: this is the input
+ * Return: 1 or 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
