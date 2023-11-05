d8e35ce6aa2700603ba187107507b3871ccd83a9
Vishnu <justvishy@gmail.com>
2023-11-05 03:15:23 -0800

    This is my fourth c program of this project

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - this finds the upper character01~
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
