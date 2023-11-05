f817cdf1f485afecc6bcb9d6922a3954db196d47
Vishnu <justvishy@gmail.com>
2023-11-05 03:05:07 -0800

    This is my third c program for this project

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - this finds the upper character
+ */
+
+void print_alphabet_x10(void)
+{
+	int alpha = 97;
+	int num = 48;
+
+	while (num <= 57)
+	{
+		alpha = 97;
+		while (alpha <= 122)
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		num++;
+		_putchar('\n');
+	}
+}
