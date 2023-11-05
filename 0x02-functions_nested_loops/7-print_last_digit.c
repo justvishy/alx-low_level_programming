4e96c8cf1a7660f0202d874c77a05672c738885b
Vishnu <justvishy@gmail.com>
2023-11-05 06:37:29 -0800

    This is my eight c program of this project

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - this is a function
+ * @n: this is the input
+ * Return: The value
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+
+	last = (n % 10);
+
+	_putchar(last + 48);
+	return (last);
+}
