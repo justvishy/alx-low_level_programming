e7280516134af84e40e0333e1f78b418882a107d
Vishnu <justvishy@gmail.com>
2023-11-05 04:10:38 -0800

    This is my seventh c program of this project

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <inttypes.h>
+
+/**
+ * _abs - this is a function
+ * @n: this is the input
+ * Return: The absolute value
+ */
+
+int _abs(int n)
+{
+	int j = n;
+	intmax_t imaxabs(intmax_t j);
+
+	if (j < 0)
+	{
+		return (-j);
+	}
+	else
+	{
+		return (j);
+	}
+}
