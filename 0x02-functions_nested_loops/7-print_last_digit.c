c831c505a44052977371e4a773b33ddd174b86fc
Vishnu <justvishy@gmail.com>
2023-11-05 16:47:38 -0800

    This is my eight c program for this project

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,12 @@ int print_last_digit(int n)
 {
 	int last;
 
-	if (n < 0)
-	{
-		n = n * -1;
-	}
-
 	last = (n % 10);
+
+	if (last < 0)
+	{
+		last = -last;
+	}
 
 	_putchar(last + '0');
 	return (last);
