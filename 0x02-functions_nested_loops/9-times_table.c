e879cab6c9b53d460d07d61ec46732406fd4b9f6
Vishnu <justvishy@gmail.com>
2023-11-05 19:07:18 -0800

    This is my tenth c program for this project

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * time_table - this is a function
+ */
+
+void times_table(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	int multiple;
+
+	while (num1 <= 9)
+	{
+		while (num2 <= 9)
+		{
+			multiple = num1 * num2;
+
+			if (num2 != 0)
+			{
+				_putchar(' ');
+			}
+
+			if (multiple > 9)
+			{
+				_putchar((multiple / 10) + 48);
+			}
+
+			if (multiple < 10)
+			{
+				_putchar(' ');
+			}
+
+			_putchar((multiple % 10) + 48);
+
+			if (num2 != 9)
+			{
+				_putchar(',');
+			}
+			num2++;
+		}
+		_putchar('\n');
+		num1++;
+		num2 = 0;
+	}
+}
