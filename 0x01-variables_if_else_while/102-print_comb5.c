34382647f04afc29f446eb019249ff956c0b4bea
Vishnu <justvishy@gmail.com>
2023-12-05 04:30:44 -0800

    My first attempt for the advanced task number 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,52 +8,30 @@
 
 int main(void)
 {
-	int num1 = 48;
-	int num2 = 48;
-	int num3 = 48;
-	int num4 = 49;
-	int set = 48;
-
-	while ((num1 <= 57 && num2 <= 57 && num3 <= 57 && num4 <= 57))
+	for (int num1 = 0; num1 < 10; num1++)
 	{
-		if (set == 49)
+		for (int num2 = 0; num2 < 10; num2++)
 		{
-			putchar(',');
-			putchar(' ');
-		}
-		putchar(num1);
-		putchar(num2);
-		putchar(' ');
-		putchar(num3);
-		putchar(num4);
-		set = 49;
+			for (int num3 = 0; num3 < 10; num3++)
+			{
+				for (int num4 = (num3 == num1) ? num2 + 1 : 0; num4 < 10; num4++)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(' ');
+					putchar(num3 + '0');
+					putchar(num4 + '0');
 
-		if (num4 <= 57)
-		{
-			num4++;
-		}
-
-		if (num4 == 58)
-		{
-			num3++;
-			num4 = 48;
-		}
-
-		if (num3 == 58 && num2 != 57)
-		{
-			num2++;
-			num3 = num1;
-			num4 = num2 + 1;
-		}
-
-		if (num2 == 57)
-		{
-			num1++;
-			num2 = 48;
-			num3 = num1;
-			num4 = num2 + 1;
+					if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
