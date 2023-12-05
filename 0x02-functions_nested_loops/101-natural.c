eef3ac26c7bdb6eacd4dc93f21a490dfae95cdbf
Vishnu <justvishy@gmail.com>
2023-12-05 12:43:59 -0800

    first attempt for this project task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 1024;
+	int x = 0;
+	int sum = 0;
+
+	while (x < 1024)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			sum += x;
+			x++;
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
