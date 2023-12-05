18b96589eda7c1e918708676f663bbd4b97f3efa
Vishnu <justvishy@gmail.com>
2023-12-05 09:20:17 -0800

    My first attempt for this question task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_table - this is a function
+ * @n: this is a number
+ */
+
+void print_times_table(int  n)
+{
+	int x = 0;
+	int y = 0;
+	int multiple;
+
+	while ((y <= n) && (n > 0 && n < 15))
+	{
+		x = 0;
+		while (x <= n)
+		{
+			multiple = y * x;
+
+			if (x > 0 && y >= 0 && multiple < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (multiple >= 10 && multiple < 100)
+			{
+				_putchar(' ');
+				_putchar(multiple / 10 + 48);
+			}
+			else if (multiple >= 100)
+			{
+				_putchar(multiple / 100 + 48);
+				_putchar(multiple / 10 % 10 + 48);
+			}
+			_putchar(multiple % 10 + 48);
+			if (x != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+	_putchar('\n');
+}
