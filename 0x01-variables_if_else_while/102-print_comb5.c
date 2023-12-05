a4a312bab343be89681cdf0de54bc438a2849450
Vishnu <justvishy@gmail.com>
2023-12-05 06:16:01 -0800

    My second attempt for the advanced task number 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,30 +8,41 @@
 
 int main(void)
 {
-	for (int num1 = 0; num1 < 10; num1++)
-	{
-		for (int num2 = 0; num2 < 10; num2++)
-		{
-			for (int num3 = 0; num3 < 10; num3++)
-			{
-				for (int num4 = (num3 == num1) ? num2 + 1 : 0; num4 < 10; num4++)
-				{
-					putchar(num1 + '0');
-					putchar(num2 + '0');
-					putchar(' ');
-					putchar(num3 + '0');
-					putchar(num4 + '0');
+	int num1 = 48;
+	int num2 = 48;
+	int num3 = 48;
+	int num4 = 49;
 
-					if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
+	for (num1; num1 <= 57; num1++)
+	{
+		for (num2; num2 <= 57; num2++)
+		{
+			for (num3; num3 <= 57; num3++)
+			{
+				for (num4; num4 <= 57; num4++)
+				{
+					if (num4 >= 50 || num3 > 48)
 					{
 						putchar(',');
 						putchar(' ');
 					}
+					putchar(num1);
+					putchar(num2);
+					putchar(' ');
+					putchar(num3);
+					putchar(num4);
 				}
+				num4 = 48;
 			}
+			num3 = num1;
+			num4 = num2 + 2;
 		}
+		num2 = 48;
+		num3 = num1 + 1;
+		num4 = num2;
+		num4 += 1;
 	}
-
 	putchar('\n');
 	return (0);
 }
+
