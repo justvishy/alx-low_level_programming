d4716eae2c7e931a1dfed9ca50f498252200e687
Vishnu <justvishy@gmail.com>
2023-12-05 23:35:07 -0800

    This is my first attempt for this project task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - this is a  function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int val1 = 1;
+	long int val2 = 2;
+	long int fib;
+	long int sum = 2;
+
+	while (fib < 4000000)
+	{
+		fib = val1 + val2;
+		val1 = val2;
+		val2 = fib;
+
+		if (val2 % 2 == 0)
+		{
+			sum += fib;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
