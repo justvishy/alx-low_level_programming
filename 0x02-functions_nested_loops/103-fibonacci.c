688e351d03e6e4bee7ba451cc012cec16e521b6f
Vishnu <justvishy@gmail.com>
2023-12-05 23:41:36 -0800

    This is my second for this project task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	long int val1 = 1;
 	long int val2 = 2;
 	long int fib;
-	long int sum = 2;
+	long int sum;
 
 	while (fib < 4000000)
 	{
@@ -24,6 +24,7 @@ int main(void)
 			sum += fib;
 		}
 	}
+	sum += 2;
 	printf("%ld\n", sum);
 	return (0);
 }
