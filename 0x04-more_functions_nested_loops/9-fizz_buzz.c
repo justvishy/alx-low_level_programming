571a0cfafbf8b2c7931d6689289ee02f4731cfb6
Vishnu <justvishy@gmail.com>
2023-12-08 13:52:21 -0800

    This is my sixth task for today in this project

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int  num = 1;
+
+	while (num < = 100)
+	{
+		if (num % 3 == 0 && num % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0 && num % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", num);
+		}
+
+		if (num != 100)
+		{
+			printf(" ");
+		}
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
