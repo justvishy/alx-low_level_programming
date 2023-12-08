8f9b3bc34bd29952f1d07432fe76a85564963c77
Vishnu <justvishy@gmail.com>
2023-12-08 12:05:04 -0800

    second task today of this project

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int repeat = 48;
+	int x = 0;
+
+	while (repeat <= 57)
+	{
+		while (x <= 9)
+		{
+			if (x > 9)
+			{
+				_putchar(x / 10 + 48);
+			}
+
+			_putchar(x % 10 + 48);
+			x++;
+		}
+		x = 0;
+		repeat++;
+		_putchar('\n');
+	}
+}
+
