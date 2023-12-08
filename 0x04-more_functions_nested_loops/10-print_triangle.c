4142cbe63e6dd723cfe6c047114bf230fbb93462
Vishnu <justvishy@gmail.com>
2023-12-08 14:46:03 -0800

    This is my seventh task for today in this project

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right angle triangle
+ *
+ * @size: This is a number
+ */
+
+void print_triangle(int size)
+{
+	int a = 1;
+	int z = 0;
+	int x = 0;
+
+	while (size >= a)
+	{
+		while (size - a > z)
+		{
+			_putchar(' ');
+			z++;
+		}
+		z = 0;
+
+		while (a > x)
+		{
+			_putchar('#');
+			x++;
+		}
+		x = 0;
+		_putchar('\n');
+		a++;
+	}
+}
