663be9b00fabf594113a3f153228dcfda0db286e
Vishnu <justvishy@gmail.com>
2023-12-14 02:40:39 -0800

    this is task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - this is a prototype function
+ *
+ * @s: this is a character
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	return(strlen(s));
+}
