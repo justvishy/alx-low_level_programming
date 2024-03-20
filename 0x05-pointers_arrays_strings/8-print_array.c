b4ea2a3e1ba75e4f4e9dfa2547c9da7f150fc7a0
Vishnu <justvishy@gmail.com>
2024-03-20 04:06:09 -0700

    edited attempt

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,19 @@ void print_array(int *a, int n)
 {
 	int num = 0;
 
-	if (n <= 0)
+	while (num > n)
 	{
 		printf("%d", a[n]);
+		
+		if (num > n + 1)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+		num--;
 	}
 
 	while (num < n)
