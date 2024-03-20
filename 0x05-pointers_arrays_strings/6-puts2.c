992754934717e0737ce9a7b92d59fa4dbeda688a
Vishnu <justvishy@gmail.com>
2024-03-20 02:44:10 -0700

    first attempt of this task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - this prints every second character
+ *
+ * @str: - this is a string
+ */
+
+void puts2(char *str)
+{
+	int length = strlen(str);
+	int start = 0;
+	int end = length;
+
+	while (start < end)
+	{
+		_putchar(str[start]);
+		start++;
+	}
+}
+
