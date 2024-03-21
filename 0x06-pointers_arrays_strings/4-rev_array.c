8b3a9fda95673f550d5c07f28b8b3a14be72df0d
Vishnu <justvishy@gmail.com>
2024-03-21 16:16:34 -0700

    first attempt

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - this reverses the array
+ *
+ * @a: this the array
+ * @n: this is the number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int num = 0;
+	int num1 = n - 1;
+	int temp;
+
+	while (num < n)
+	{
+		temp = a[num];
+		a[num] = a[num1];
+		a[num1] = temp;
+		num++;
+		num1--;
+	}
+}
