b47bb1134250671d3af7207788428147f3ebc793
Vishnu <justvishy@gmail.com>
2024-03-21 15:41:31 -0700

    first attempt

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - this is a function
+ *
+ * @s1: input 1
+ * @s2: input 2
+ * Return: number
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int sum = s1 - s2;
+
+	return (sum);
+}
