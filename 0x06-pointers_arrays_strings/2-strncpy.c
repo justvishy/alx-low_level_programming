57117eaf23e8b90c6ec6c8d386f3d568a9df7c17
Vishnu <justvishy@gmail.com>
2024-03-21 14:51:53 -0700

    first attempt

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - this copied the string
+ *
+ * @dest: this is a string
+ * @src: this is a string
+ * @n: this is an integer
+ *
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int num = 0;
+
+	while (num < n && src[num] != '\0')
+	{
+		dest[num] = src[num];
+		num++;
+	}
+	Return (dest);
+}
