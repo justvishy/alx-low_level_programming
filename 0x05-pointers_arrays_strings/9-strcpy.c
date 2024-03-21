3d770516bb9ca1d593fee582ea53e5237f2ce7c1
Vishnu <justvishy@gmail.com>
2024-03-21 12:51:31 -0700

    First attempt

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - this copies a string
+ *
+ * @dest: this is a string
+ * @src: this is a string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int num = 0;
+
+	while (src[num] != '\0')
+	{
+		dest[num] = src[num];
+		num++;
+	}
+	dest[num] = '\0';
+
+	return dest;
+}
