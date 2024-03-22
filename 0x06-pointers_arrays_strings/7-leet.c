fe39cb4835e37ae79cf071c1a1878adba1be3689
Vishnu <justvishy@gmail.com>
2024-03-22 03:47:41 -0700

    First Attempt

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * leet - writes a code that encodes a string to 1337
+ *
+ * @z: string
+ *
+ * Return: string
+ */
+
+char *leet(char *z)
+{
+	int length = strlen(z);
+	int start = 0;
+
+	while (start < length)
+	{
+		if (z[start] == 'a' || z[start] == 'A')
+		{
+			z[start] = 4;
+		}
+		else if (z[start] == 'e' || z[start] == 'E')
+		{
+			z[start] = 3;
+		}
+		else if (z[start] == 'o' || z[start] == 'O')
+		{
+			z[start] = 0;
+		}
+		else if (z[start] == 't' || z[start] == 'T')
+		{
+			z[start] = 7;
+		}
+		else if (z[start] == 'l' || z[start] == 'L')
+		{
+			z[start] = 1;
+		}
+
+		start++;
+	}
+	return (z);
+}
