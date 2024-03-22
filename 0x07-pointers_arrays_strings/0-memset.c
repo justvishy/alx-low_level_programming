a81bf4a4b129197f82e87d1d473697390a765d8f
Vishnu <justvishy@gmail.com>
2024-03-22 13:34:56 -0700

    First Attempt

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * _memset - fills memory with a constant byte.
+ *
+ * @s - char
+ * @b - char
+ * @n - unsigned int
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int z = 0;
+
+	while (z < n)
+	{
+		s[z] = b;
+		++z;
+	}
+	return (s);
+}
