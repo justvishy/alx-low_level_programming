59f0f621ef737fd8782fc6810681128d8ba9d5fa
Vishnu <justvishy@gmail.com>
2024-03-22 01:29:39 -0700

    First Attempt

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - capitalizes the space
+ *
+ * @z: string
+ *
+ * Return: the completed string
+ */
+
+char *cap_string(char *z)
+{
+	int length = strlen(z);
+	int start = 0;
+
+	while (start < length)
+	{
+		if (z[start] == " " || z[start] == '\t' || z[start] == '\n')
+		{
+			z[start + 1] = toupper(z[start + 1]);
+		}
+		else if (z[start] == ',' || z[start] == ';' || z[start] == '.' || z[start] == '!' || z[start] == '?')
+		{
+			z[start + 1] = toupper(z[start + 1]);
+		}
+		else if (z[start] == '"' || z[start] == '(' || z[start] == ')' || z[start] == '{' || z[start] == '}')
+		{
+			z[start + 1] = toupper(z[start + 1]);
+		}
+		
+		start++;
+	}
+	return (z);
+}
