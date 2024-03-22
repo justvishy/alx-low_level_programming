173c13532603f12caf0b0597f134995ceef6c42c
Vishnu <justvishy@gmail.com>
2024-03-22 13:45:28 -0700

    First Attempt

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * _memcpy - copies memory area.
+ *
+ * @dest: char
+ * @src: char
+ * @n: unsigned int
+ *
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int z = 0;
+
+	while (z < n)
+	{
+		src[z] = dest[z];
+		z++;
+	}
+	return (dest);
+}
