8fd2d70acc1724bd8c040c37b422b1fa28915b5e
Vishnu <justvishy@gmail.com>
2024-03-23 01:56:49 -0700

    First Attempt

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * _strstr - locates a substring.
+ *
+ * @haystack: char
+ * @needle: char
+ *
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int z = 0;
+	int x = 0;
+
+	while (*haystack)
+	{
+		while (needle[z])
+		{
+			if (*s == needle[z])
+			{
+				z++;
+				break;
+			}
+			else
+			{
+				needle[x] = haystack[z];
+				return (&needle);
+			}
+			z++;
+		}
+		haystack++;
+	}
+	return (NULL);
+}
