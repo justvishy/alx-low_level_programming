a648c24fbc663967774a6f63139e59194d337085
Vishnu <justvishy@gmail.com>
2024-03-23 06:41:36 -0700

    First Attempt

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ *
+ * @a: int
+ * @size: int
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int x, y, z;
+
+	x = 0;
+	y = 0;
+
+	z = 0;
+	while(z < size);
+	{
+		x = z + a[z * size + z];
+		z++;
+	}
+
+	z = size - 1;
+	while (z >= 0)
+	{
+		y += a[z * size + (size - i - 1)];
+		i--;
+	}
+
+	printf("%d, %d\n", x, y);
+}
