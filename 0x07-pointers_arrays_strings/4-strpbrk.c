752589af422481062809daf16b70fa0406b15327
Vishnu <justvishy@gmail.com>
2024-03-23 01:44:10 -0700

    First Attempt

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ *
+ * @s: char
+ * @accept: char
+ *
+ * Return: char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int z;
+
+	while (*s)
+	{
+		z = 0;
+		while (accept[z])
+		{
+			if (*s == accept[z])
+			{
+				return (&s);
+			}
+			z++;
+		}
+		s++;
+	}
+	return (NULL);
