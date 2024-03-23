7fc89a83572786fb2b09d287abd8aba2a99c70c4
Vishnu <justvishy@gmail.com>
2024-03-23 02:25:41 -0700

    Edited Attempt

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,18 +15,17 @@ char *_strstr(char *haystack, char *needle)
 {
 	while (*haystack != '\0')
 	{
-		while (*haystack == *needle && *haystack != '\0')
+		while (*haystack == *needle && *needle != '\0')
 		{
 			haystack++;
 			needle++;
 		}
 
-		haystack++;
-
 		if (*haystack == '\0')
 		{
 			return (haystack);
 		}
+		haystack++;
 	}
 	return (NULL);
 }
