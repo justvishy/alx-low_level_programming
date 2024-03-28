0c0e4e03de3014ac0f82e500bcad2fa66bf2ab5c
Vishnu <justvishy@gmail.com>
2024-03-28 05:40:58 -0700

    First Attempt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds the squareroot
+ *
+ * @n: int
+ *
+ * Return: result
+ */
+
+int _sqrt_recursion(int n)
+{
+	static int z = 1;
+
+	if (z * z == n)
+	{
+		return (z);
+	}
+	else if (z > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		z += 1;
+		int final = _sqrt_recursion(n);
+
+		z = 0;
+		return (final);
+	}
+}
