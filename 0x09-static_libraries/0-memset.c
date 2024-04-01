bd5d848af3bab3adf7001142c8a6a00835abca26
Vishnu <justvishy@gmail.com>
2024-04-01 11:15:09 -0700

    Checking

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ *
+ * @s: char
+ * @b: char
+ * @n: unsigned int
+ * Return: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
