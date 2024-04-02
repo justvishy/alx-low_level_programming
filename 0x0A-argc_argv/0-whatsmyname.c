173e482e153454885aea65b819106c474e294f5c
Vishnu <justvishy@gmail.com>
2024-04-01 23:24:16 -0700

    First Attempt

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - this is a main function
+ *
+ * @argc: int
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
