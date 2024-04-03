51ed9913b0401164c4dc43fb6c8b20e4a337325e
Vishnu <justvishy@gmail.com>
2024-04-03 16:52:25 -0700

    First Attempt

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * @argc: int
+ * @argv: char
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv)
+{
+	while (*argv)
+	{
+		printf("%s\n", *argv);
+	}
+
+	return (0);
+}
