2c0a0640c180b8053925e4f8008a1fbc285290ef
Vishnu <justvishy@gmail.com>
2024-04-03 16:47:27 -0700

    Edited Attempt

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,11 +13,12 @@ int main(int argc __attribute__((unused)), char *argv[])
 {
 	int count = 0;
 
-	while (*argv && *argv != '\0')
+	while (*argv)
 	{
 		argv++;
 		count++;
 	}
+	count -= 1;
 
 	printf("%d\n", count);
 
