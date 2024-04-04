5d51ca194f015ae7cc3a98474e655bd8f346d480
Vishnu <justvishy@gmail.com>
2024-04-03 18:46:43 -0700

    Edited Attempt

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - function
@@ -15,23 +16,30 @@ int main(int argc, char *argv[])
 {
 	int result = 0;
 	int count = 1;
+	int check = 0;
 
 	if (argc > 2)
 	{
 		while (count < argc)
 		{
-			result += atoi(argv[count]);
+			while (argv[count])
+			{
+				if (isdigit(*argv[check]) && *argv[check] != '\0')
+				{
+					check++;
+				}
+				else if (*argv[check] == '\0')
+				{
+					result = += atoi(argv[count]);
+					break;
+				}
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 			count++;
-		}
-
-		if (result > 0)
-		{
-			printf("%d\n", result);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
 		}
 	}
 	else
