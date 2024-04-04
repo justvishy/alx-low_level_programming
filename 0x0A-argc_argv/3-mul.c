a7f8cda2e2151aa9d521fa76bbef23d7b251f76a
Vishnu <justvishy@gmail.com>
2024-04-03 17:23:19 -0700

    Edited Attempt

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,7 +15,7 @@ int main(int argc , char *argv[])
 
 	if (argc > 1)
 	{
-		result = (argv[1] - '0') * (argv[2] - '0');
+		result = (*argv[1] - '0') * (*argv[2] - '0');
 		printf("%d\n", result);
 	}
 	else
