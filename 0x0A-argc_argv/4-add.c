23a45966eda2278c9f93bb85fe431a97ca0c2562
Vishnu <justvishy@gmail.com>
2024-04-03 19:14:56 -0700

    Edited Attempt

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 			}
 			else if (argv[count][check] == '\0')
 			{
-				return += atoi(argv[count]);
+				result += atoi(argv[count]);
 				count++;
 				check = 0;
 			}
