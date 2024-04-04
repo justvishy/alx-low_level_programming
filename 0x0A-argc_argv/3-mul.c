4b2aaa2261d5f5dce77f6635a7729eed2ae434c3
Vishnu <justvishy@gmail.com>
2024-04-03 17:19:37 -0700

    First Attempt

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - funcrion
+ *
+ * *argc: int
+ * *argv: char
+ * Return: 0 or 1
+ */
+
+int main(int argc , char *argv[])
+{
+	int result = 1;
+
+	if (argc > 1)
+	{
+		result = (argv[1] - '0') * (argv[2] - '0');
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}		
