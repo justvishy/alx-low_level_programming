187e50ef7badc7b0c2579f6cb9f285275d2c730a
Vishnu <justvishy@gmail.com>
2024-04-13 03:40:00 -0700

    First Attempt

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - this creates an array
+ *
+ * @size: size of the array
+ * @c: this is the specific character
+ *
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int n = 0;
+	char* ptr;
+
+	ptr = (char*) malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else if (size == 0)
+	{
+		return (NULL);
+	}
+
+	while (n < size)
+	{
+		ptr[n] = c;
+		n++;
+	}
+	return (ptr);
+	free(ptr);
+}
