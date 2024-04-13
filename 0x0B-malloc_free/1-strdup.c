2ab93b64b4c9d0bd142ea76999d35b75aa574925
Vishnu <justvishy@gmail.com>
2024-04-13 04:04:49 -0700

    First Attempt

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - this duplicates
+ *
+ * @str: string
+ * Return: the duplicated string
+ */
+
+int main(void)
+{
+	char *ptr;
+	int n = 0;
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*str)
+	{
+		ptr[n] = *str;
+		str++;
+		n++;
+	}
+	return (ptr);
+	free(str);
+}
