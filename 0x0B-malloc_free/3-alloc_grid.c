01c6c75593505974a470c5852748169ab78f4524
Vishnu <justvishy@gmail.com>
2024-04-15 06:34:03 -0700

    FIrst Attempt

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * alloc_grid - prints a grid
+ *
+ * @width - this is the width
+ * @height - this is the height
+ *
+ * Return: int
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ar;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ar = malloc(sizeof(int) * height);
+
+	if (ar == NULL)
+		return (NULL);
+
+	x = 0;
+	while (x < height)
+	{
+		ar[x] = malloc(sizeof(int) * width);
+
+		if (ar[x] == NULL)
+			return (NULL);
+
+		y = 0;
+		while (y < width)
+		{
+			ar[x][y] = 0;
+			y++;
+		}
+		x++;
+	}
+	return (ar);
+}
