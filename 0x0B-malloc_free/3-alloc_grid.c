08a7a59adc101d1324dde7f423e20c01b6a7928e
Vishnu <justvishy@gmail.com>
2024-04-15 06:39:15 -0700

    Edited Attempt

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,8 +4,8 @@
 /**
  * alloc_grid - prints a grid
  *
- * @width - this is the width
- * @height - this is the height
+ * @width: - this is the width
+ * @height: - this is the height
  *
  * Return: int
  */
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ar = malloc(sizeof(int) * height);
+	ar = malloc(sizeof(int *) * height);
 
 	if (ar == NULL)
 		return (NULL);
