1fb83aeed809a7152dc6126bb4c03b911e339cb6
Vishnu <justvishy@gmail.com>
2024-04-15 05:29:41 -0700

    Edited Attempt

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,32 +18,27 @@ char *str_concat(char *s1, char *s2)
 	int n = 0;
 	int o = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return ("");
-	else if (s2 == NULL)
-		return (s1);
-	else if (s1 == NULL)
-		return (s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if (s1 != NULL && s2 != NULL)
+	sum = (strlen(s1) + strlen(s2) + 1);
+	ptr = (char *) malloc(sum * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	while  (s1[n] != '\0')
 	{
-		sum = (strlen(s1) + strlen(s2) + 1);
-		ptr = (char *) malloc(sum * sizeof(char));
-
-		if (ptr == NULL)
-			return (NULL);
-
-		while  (s1[n] != '\0')
-		{
-			ptr[n] = s1[n];
-			n++;
-		}
-		while (s2[o] != '\0')
-		{
-			ptr[n + o] = s2[o];
-			o++;
-		}
-		ptr[n + o] = '\0';
+		ptr[n] = s1[n];
+		n++;
 	}
+	while (s2[o] != '\0')
+	{
+		ptr[n + o] = s2[o];
+		o++;
+	}
+	ptr[n + o] = '\0';
 	return (ptr);
 }
