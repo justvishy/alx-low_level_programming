5d7340aba93a1ff16d2cc4567cb7945c41b5a067
Vishnu <justvishy@gmail.com>
2024-04-15 07:35:41 -0700

    First Attempt

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - checks memory
+ * 
+ * @b: unsigned int
+ */
+
+void **malloc_checked(unsigned int b)
+{
+	int x;
+	char *ptr;
+
+	x = (int)(b);
+	ptr = (char *) malloc(x * sizeof(char));
+
+	if (ptr == NULL)
+		exit(98);
+}
