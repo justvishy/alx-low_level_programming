09e56122b3d3e0bdfcb7cd042d7e4924a221dabd
Vishnu <justvishy@gmail.com>
2024-04-15 07:09:55 -0700

    First Attempt

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * free_grid - clears the memory
+ *
+ * @grid: the array
+ * @height: the height
+ */
+
+void free_grid(int **grid, int height)
+{
+	int y;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	y = 0;
+	while (y < height)
+	{
+		if (grid[y] != NULL)
+			free(grid[y]);
+
+		y++;
+	}
+	free(grid);
+}
