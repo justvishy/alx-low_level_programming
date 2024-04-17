367a62cf9c9ce25fc8a391072e266028136c15a2
Vishnu <justvishy@gmail.com>
2024-04-17 03:55:04 -0700

    First Attempt

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function clears memory
+ *
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ *
+ * Return: ptr
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr, x, y, z;
+
+	x = (int)(nmemb);
+	y = (int)(size);
+
+	if (x <= 0)
+		return (NULL);
+	if (y <= 0)
+		return (NULL);
+
+	ptr = (int *) malloc(x * sizeof(size));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	z = 0;
+	while (z < x)
+	{
+		ptr[z] = 0;
+		z++;
+	}
+	return (ptr);
+}
