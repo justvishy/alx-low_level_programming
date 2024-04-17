4fa8ee1faac08f6ebf619f26f4d831d1252e6902
Vishnu <justvishy@gmail.com>
2024-04-17 06:12:05 -0700

    First Attempt

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *filename = __FILE__;
+
+	while (*filename)
+	{
+		_putchar(*filename);
+		*filename++;
+	}
+	_putchar('\n');
+	return (0);
+}
