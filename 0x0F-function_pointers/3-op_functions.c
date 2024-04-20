3b139af57e5590c80ce40efdf805b62249804ca2
Vishnu <justvishy@gmail.com>
2024-04-20 08:09:47 -0700

    First Attempt

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds
+ *
+ * @a: val1
+ * @b: val2
+ *
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ *
+ * @a: val1
+ * @b: val2
+ *
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ *
+ * @a: val1
+ * @b: val2
+ *
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ *
+ * @a: val1
+ * @b: val2
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ *
+ * @a: val1
+ * @b: val2
+ *
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
