493b51155a53d2f43f3cbe56e7361b605071483a
Vishnu <justvishy@gmail.com>
2024-04-20 01:52:24 -0700

    First Attempt

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - this free dogs
+ *
+ * @d: struct of dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	if (d != NULL)
+		free(d);
+}
