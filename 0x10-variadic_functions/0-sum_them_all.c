0ff2ac779b51cdd851335c2e381f5891870012ea
Vishnu <justvishy@gmail.com>
2024-04-21 03:13:44 -0700

    First Attempt

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - adds all the numbers input
+ *
+ * @n: numbers input
+ * Return: the sum
+ */
+
+int sum_them_al(const unsigned int n, ...)
+{
+	va_list args;
+	int m = (int)(n), i;
+	unsigned int sum = 0;
+
+	va_start(args, n);
+	
+	if (n == 0)
+		return (0);
+
+	i = 0;
+	while (i < m)
+	{
+		sum += va_arg(args, int);
+		i++;
+	}
+
+	va_end(args);
+	return (sum);
+}
