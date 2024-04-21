1972990af3adebdb92d9f7aaab0acbe3f1d46b35
Vishnu <justvishy@gmail.com>
2024-04-21 05:38:14 -0700

    First Attempt

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - this prints all type inputs
+ *
+ * @format: input
+ */
+
+void print_all(const char *const format, ...)
+{
+	char *str;
+	char *separ = "";
+	va_list args;
+
+	va_start(args, format);
+
+	i = 0;
+	while (format[i])
+	{
+		switch(format[i])
+		{
+			case 'c':
+				printf("%s%c", separ, (char)va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separ, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separ, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+
+				if (str != NULL)
+					printf("%s%s", separ, str);
+				if (str == NULL)
+					printf("(nil)");
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separ = ", ";
+		i++;
+	}
+	va_end(args);
+}
